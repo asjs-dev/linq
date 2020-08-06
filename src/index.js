268de583cedbb6ebac3003e62b371e130b97e763
asjs-dev <asjs-dev@users.noreply.github.com>
2020-08-06 11:47:52 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -98,6 +98,16 @@ Array.prototype.groupBy = function(callback) {
   return response;
 };
 
+Array.prototype.shuffle = function() {
+  for (let i = this.length - 1; i > 0; --i){
+    const j = Math.floor(Math.random() * i);
+    const temp = this[i];
+    this[i] = this[j];
+    this[j] = temp;
+  }
+  return this;
+};
+
 Array.prototype.first = function() {
   return this[0];
 };
